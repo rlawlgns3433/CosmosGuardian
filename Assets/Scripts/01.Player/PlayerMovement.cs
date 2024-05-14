276d374b5b3b6014c5e0d47e12fade86b74e1ced
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-05-14 18:01:17 +0900

    주석 제거

--- Assets/Scripts/01.Player/PlayerMovement.cs
+++ Assets/Scripts/01.Player/PlayerMovement.cs
@@ -12,43 +12,6 @@ public class PlayerMovement : MonoBehaviour
     private float minX = -4.05f;
     private float maxX = 4.05f;
 
-
-    //    private void Update()
-    //    {
-    //        // 플레이어 X축 이동은 Axis와 PlayerStats의 Horizontal Speed의 곱으로 사용
-    //        // 현재 임시 속력 사용
-    //#if UNITY_ANDROID
-    //        rb.MovePosition(transform.position 
-    //            + (forward * playerStats.stats[CharacterColumn.Stat.MOVE_SPEED_V] * verticalCoefficient + Vector3.right * dynamicJoystick.Horizontal * playerStats.stats[CharacterColumn.Stat.MOVE_SPEED_H] * horizontalCoefficient) * Time.deltaTime);
-    //#elif UNITY_EDITOR_WIN
-    //        rb.MovePosition(transform.position 
-    //        + (forward * playerStats.speedVertical * verticalCoefficient + Vector3.right * dynamicJoystick.Horizontal * playerStats.speedHorizontal * horizontalCoefficient) * Time.deltaTime);
-    //#endif
-    //        //if(transform.position.x < minX)
-    //        //{
-    //        //    transform.position = new Vector3(minX, transform.position.y, transform.position.z);
-    //        //}
-
-    //        //if (transform.position.x > maxX)
-    //        //{
-    //        //    transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
-    //        //}
-
-    //        if (transform.position.x < minX)
-    //        {
-    //            Vector3 newPosition = transform.position;
-    //            newPosition.x = minX;
-    //            transform.position = newPosition;
-    //        }
-
-    //        if (transform.position.x > maxX)
-    //        {
-    //            Vector3 newPosition = transform.position;
-    //            newPosition.x = maxX;
-    //            transform.position = newPosition;
-    //        }
-    //    }
-
     private void FixedUpdate()
     {
         // 이동 거리 계산

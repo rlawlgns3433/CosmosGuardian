b178a465103ef163279de323c5c5a7aaa63bdfdc
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-05-17 17:59:25 +0900

    일시정지 UI에 플레이어 스텟 데이터 추가

--- Assets/UiController.cs
+++ Assets/UiController.cs
@@ -1,14 +1,33 @@
+using TMPro;
 using UnityEngine;
 
 public class UiController : MonoBehaviour
 {
+    private static readonly string statFormat = "데미지 : {0}\n공격빈도 : {1}회/min\n투사체 속도 : {2}\n사거리 : {3}\n크리티컬 확률 : {4}%\n크리티컬 데미지 : {5}%\n관통 : {6}개\n스플래시 범위 : {7}\n스플래시 데미지 : {8}%\n체력 흡수 : {9}%\n투사체 개수 : {10}개\n";
+
     [Header("Group")]
     public GameObject group;
     public GameObject pause;
+    public TextMeshProUGUI textStats;
+    public PlayerStats playerStats;
+    public PlayerShooter playerShooter;
 
     private void Start()
     {
         group.SetActive(true);
         pause.SetActive(false);
+
+        textStats.text = string.Format(statFormat, 
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.DAMAGE] * playerStats.stats[CharacterColumn.Stat.DAMAGE]),
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.FIRE_RATE] * playerStats.stats[CharacterColumn.Stat.FIRE_RATE]),
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.PROJECTILE_SPEED] * playerStats.stats[CharacterColumn.Stat.PROJECTILE_SPEED]),
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.FIRE_RANGE] * playerStats.stats[CharacterColumn.Stat.FIRE_RANGE]),
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.CRITICAL] * playerStats.stats[CharacterColumn.Stat.CRITICAL]),
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.CRITICAL_DAMAGE] * playerStats.stats[CharacterColumn.Stat.CRITICAL_DAMAGE]), 
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.PENETRATE] * playerStats.stats[CharacterColumn.Stat.PENETRATE]),
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.SPLASH_RANGE] * playerStats.stats[CharacterColumn.Stat.SPLASH_RANGE]),
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.SPLASH_DAMAGE] * playerStats.stats[CharacterColumn.Stat.SPLASH_DAMAGE]),
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.HP_DRAIN] * playerStats.stats[CharacterColumn.Stat.HP_DRAIN]), 
+            (int)(playerShooter.weapon.stats[WeaponColumn.Stat.PROJECTILE_AMOUNT] * playerStats.stats[CharacterColumn.Stat.PROJECTILE_AMOUNT]));
     }
 }

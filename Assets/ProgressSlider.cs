3d960c14508492af221d665df674e981abe24e46
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-05-20 16:12:59 +0900

    스테이지 마다 슬라이더 초기화

--- Assets/ProgressSlider.cs
+++ Assets/ProgressSlider.cs
@@ -1,4 +1,3 @@
-using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,13 +11,17 @@ public class ProgressSlider : MonoBehaviour
     // platform distance = 27
     // BossSpawnController의 bossSpawnThreshold = 3
 
+    private int stage = 1;
     private int startPlatformDistance = 27;
     private int pooledPlatformCount;
-    private int stopDistanceAtBoss = 12;
+    private int stopDistanceAtBoss = 15;
     private int platformDistance = 27;
     private int totalDistance = 0;
     private int appearanceThreshold;
     private float progressRate = 0f;
+    private float movedDistance = 0f;
+    private float stageLength = 0f;
+    private int distanceToNextStage = 12;
 
     public BossSpawnController bossSpawnController;
 
@@ -28,16 +31,32 @@ public class ProgressSlider : MonoBehaviour
         appearanceThreshold = bossSpawnController.bossAppearanceThreshold;
         totalDistance = startPlatformDistance + (appearanceThreshold + 1) * platformDistance * pooledPlatformCount + stopDistanceAtBoss;
         progressRate = playerMovement.transform.position.z / totalDistance;
-
-        Debug.Log(progressRate);
     }
 
     private void Update()
     {
-        if(Time.frameCount % 3 == 0)
+        if (Time.frameCount % 3 == 0)
         {
-            progressRate = playerMovement.transform.position.z / totalDistance;
+            if(stage == 1)
+            {
+                movedDistance = playerMovement.transform.position.z;
+            }
+            else
+            {
+                movedDistance = playerMovement.transform.position.z - stageLength;
+            }
+            progressRate = movedDistance / totalDistance;
             progressSlider.value = progressRate;
         }
+    }
+
+    // 스테이지 클리어 시 호출 필요
+    public void ResetProgressSlider()
+    {
+        ++stage;
+        // 플레이어 위치에서 totalDistance 뺀 값을 설정
+        stageLength += movedDistance;
+        movedDistance = 0; // 플레이어가 이동한 거리
+        totalDistance = distanceToNextStage + (appearanceThreshold + 1) * platformDistance * (pooledPlatformCount - 1)+ stopDistanceAtBoss; // 다음 이동할 거리
     }
 }
